dba48b958db34a225ee27977d09e25dd9e5b11e6
Zach <bp@bps-server.xyz>
2023-02-22 23:19:31 -0800

    Almost done

--- lab5.asm
+++ lab5.asm
@@ -15,6 +15,8 @@
 .def	waitcnt = r17				; Wait Loop Counter
 .def	ilcnt = r18				; Inner Loop Counter
 .def	olcnt = r19				; Outer Loop Counter
+.def	lcnt = r3				; Left bumper counter
+.def	rcnt = r4				; Right bumper counter
 
 .equ	WTime = 200				; Time to wait in wait loop
 
@@ -48,12 +50,10 @@
 		rjmp	INIT		; Jump to program initialization
 
 .org	$0002
-		rcall	HitRight	; Call right whisker
-		reti	
+		rjmp	HitRight	; Call right whisker
 
 .org	$0004
-		rcall	HitLeft		; Call left whisker
-		reti
+		rjmp	HitLeft		; Call left whisker
 
 .org	$002E				; End of Interrupt Vectors
 ;--------------------------------------------------------------
@@ -89,6 +89,16 @@ INIT:
 	; Enable the external pins to recive the values
 		ldi		mpr, 0b00001011	;enable both wiskers for when they are hit
 		out		EIMSK, mpr
+
+	; Init the LCD
+		RCALL LCDInit
+		RCALL LCDClr
+		RCALL LCDBacklightOn
+	; Clear the Counters
+		CLR lcnt
+		CLR rcnt
+	; Show the counter
+		RCALL SHOW_COUNT
 
 	; Enable interrupts
 		;should be last thing in INIT, lest we wish to anger the assembly gods
@@ -133,6 +143,7 @@ MAIN:
 ;		is triggered.
 ;----------------------------------------------------------------
 HitRight:
+		INC		rcnt		; Increase the amount of hits.
 		push	mpr			; Save mpr register
 		push	waitcnt			; Save wait register
 		in		mpr, SREG	; Save program state
@@ -142,7 +153,8 @@ HitRight:
 		ldi		mpr, MovBck	; Load Move Backward command
 		out		PORTB, mpr	; Send command to port
 		ldi		waitcnt, WTime	; Wait for 1 second
-		rcall	Wait			; Call wait function
+		RCALL HIT_SCREEN	; Show that it's been hit
+		RCALL	SHOW_COUNT	; Show the counter again.
 
 		; Turn left for a second
 		ldi		mpr, TurnL	; Load Turn Left Command
@@ -155,14 +167,14 @@ HitRight:
 		
 
 		;clear queue for interrupts
-		ldi		mpr,0b00000000
+		ldi		mpr,$00
 		out		EIFR,mpr	;clear the interrupt queue to account for excess calls
 
 		pop		mpr		; Restore program state
 		out		SREG, mpr	;
 		pop		waitcnt		; Restore wait register
 		pop		mpr		; Restore mpr
-		ret				; Return from subroutine
+		reti				; Return from subroutine
 
 ;----------------------------------------------------------------
 ; Sub:	HitLeft
@@ -170,6 +182,7 @@ HitRight:
 ;		is triggered.
 ;----------------------------------------------------------------
 HitLeft:
+		INC	lcnt			; Increase the amount of hits.
 		push	mpr			; Save mpr register
 		push	waitcnt			; Save wait register
 		in		mpr, SREG	; Save program state
@@ -179,7 +192,8 @@ HitLeft:
 		ldi		mpr, MovBck	; Load Move Backward command
 		out		PORTB, mpr	; Send command to port
 		ldi		waitcnt, WTime	; Wait for 1 second
-		rcall	Wait			; Call wait function
+		RCALL HIT_SCREEN	; Show that it's been hit
+		RCALL	SHOW_COUNT	; Show the counter again.
 
 		; Turn right for a second
 		ldi		mpr, TurnR	; Load Turn Left Command
@@ -191,14 +205,146 @@ HitLeft:
 		out		PORTB, mpr	; Send command to port
 
 		;clear queue for interrupts
-		ldi	mpr,0b00000001
+		ldi	mpr,$00
 		out	EIFR,mpr	;clear the interrupt queue to account for excess calls
 
 		pop		mpr		; Restore program state
 		out		SREG, mpr	;
 		pop		waitcnt		; Restore wait register
 		pop		mpr		; Restore mpr
-		ret				; Return from subroutine
+		reti				; Return from subroutine
+
+;----------------------------------------------------------------
+; Func: SHOW_COUNT
+; Desc: Get the count for each hit and display it on the screen
+;----------------------------------------------------------------
+SHOW_COUNT:
+	PUSH XL
+	PUSH XH
+	PUSH ZL
+	PUSH ZH
+	PUSH mpr
+	PUSH ilcnt
+	; Load in the text for the top
+	LDI ZL, LOW(STR_CNT_TOP << 1)	; Program memory of the string
+	LDI ZH, HIGH(STR_CNT_TOP << 1)
+	LDI XL, $00 ; Data memory of the buffer
+	LDI XH, $01
+
+	; Put the string into the
+	LDI ilcnt, 6
+	TOP_STRING_LOOP:
+		LPM mpr, Z+
+		ST X+, mpr
+		DEC ilcnt
+		BRNE TOP_STRING_LOOP
+
+	; Load in the count
+	MOV mpr, lcnt
+	; Convert to ASCII
+	RCALL BIN2ASCII
+
+	; Load in the text for the bottom
+	LDI ZL, LOW(STR_CNT_BOT << 1)	; Program memory of the string
+	LDI ZH, HIGH(STR_CNT_BOT << 1)
+	LDI XL, $10 ; Data memory of the buffer
+	LDI XH, $01
+
+	; Put the string into the
+	LDI ilcnt, 7
+	BOT_STRING_LOOP:
+		LPM mpr, Z+
+		ST X+, mpr
+		DEC ilcnt
+		BRNE BOT_STRING_LOOP
+
+	; Load in the count
+	MOV mpr, rcnt
+	; Convert to ASCII
+	RCALL BIN2ASCII
+
+	; Render the screen.
+	RCALL LCDWrite
+
+	POP ilcnt
+	POP mpr
+	POP ZH
+	POP ZL
+	POP XH
+	POP XL
+
+	RET
+
+
+;----------------------------------------------------------------
+; Func: HIT_SCREEN
+; Desc: Clears the screen and flashes.
+;----------------------------------------------------------------
+HIT_SCREEN:
+	PUSH ilcnt	; Counter for waiting .25 seconds
+	PUSH olcnt	; Counter for how many flashes
+	PUSH mpr	; State of the LCD backlight.
+	PUSH XL
+	PUSH XH
+	PUSH ZL
+	PUSH ZH
+	; Clear the screen
+	RCALL LCDClr
+
+	; Load in the Ouch text!
+	LDI ZL, LOW(STR_HIT_STRING << 1)
+	LDI ZH, HIGH(STR_HIT_STRING << 1)
+	; Load in the LCD buffer
+	LDI XL, $05
+	LDI XH, $01
+	; Loop count
+	LDI olcnt, 5
+	HIT_LOAD_LOOP:
+		LPM mpr, Z+
+		ST X+, mpr
+		DEC olcnt
+		BRNE HIT_LOAD_LOOP
+
+	; Render the LCD
+	RCALL LCDWrite
+
+	LDI mpr, 1	; Set to 1, as it's on by default.
+	LDI olcnt, 8; Clear out outer count
+	FLASH_LOOP:
+		LDI ilcnt, 250
+		CPI mpr, 0
+		; Check to see if we should turn on or turn off the display.
+		BREQ TURN_ON
+			; Turn off the display
+			RCALL LCDBacklightOff
+			DEC mpr ; Set mpr to 0
+			RJMP LCD_END
+		TURN_ON:
+			; Turn on the display
+			RCALL LCDBacklightOn
+			INC mpr ; Set mpr to 1
+		LCD_END:
+		; Loop 250 times to wait 1/4 second
+		RCALL LCDDelay
+		DEC ilcnt
+		BRNE LCD_END
+		; Decrement the outer count and see if we should break out
+		DEC olcnt
+		BRNE FLASH_LOOP
+	; Done, make sure the LCD is on and the screen is cleared
+	RCALL LCDBacklightOn
+	RCALL LCDClr
+
+	POP ZH
+	POP ZL
+	POP XH
+	POP XL
+	POP mpr
+	POP olcnt
+	POP ilcnt
+
+	RET
+
 
 ;----------------------------------------------------------------
 ; Sub:	Wait
@@ -228,3 +374,12 @@ ILoop:	dec		ilcnt			; decrement ilcnt
 		ret				; Return from subroutine
 
 .include "LCDDriver.asm"
+
+STR_CNT_TOP:
+.DB		"Left: "
+STR_CNT_BOT:
+.DB		"Right:  "
+STR_HIT_STRING:
+.DB		"Ouch! "
+STR_END:
+
